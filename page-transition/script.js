9ec0341d66e9b123ee3dc822865d119a094de9f5
chan <penalescjay@gmail.com>
2025-01-17 00:37:52 +0800

    qeqw

--- page-transition/script.js
+++ page-transition/script.js
@@ -20,9 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
     gsap.set(".block", { visibility: "hidden" });
   });
   
-  animateTitle().then(() => {
-    gsap.set(".hero h1", { visibility: "visible" }); // Hide after animation
-  });
+  // animateTitle().then(() => {
+  //   gsap.set(".hero h1", { visibility: "visible" }); 
+  // });
 
 
   function revealTransition() {
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
           grid: "auto",
           axis: "x",
         },
-        ease: ease,
+        ease: "power4.inOut",
         onComplete: resolve,
       });
     });
@@ -54,33 +54,33 @@ document.addEventListener("DOMContentLoaded", () => {
           grid: "auto",
           axis: "x",
         },
-        ease: ease,
+        ease: "power4.inOut",
         onComplete: resolve,
       });
     });
   }
-  function animateTitle() {
-    return new Promise((resolve) => {
-      // Set initial properties
-      gsap.set(".hero h1", { visibility: "visible", scale: 0.5, opacity: 0 });
+  // function animateTitle() {
+  //   return new Promise((resolve) => {
+  //     // Set initial properties
+  //     gsap.set(".hero h1", { visibility: "visible", scale: 0.5, opacity: 0 });
   
-      // Animate scaling and opacity
-      gsap.to(".hero h1", {
-        scale: 1.5, // Scale uniformly in all directions
-        opacity: 1, // Fade in
-        duration: 1, // Animation duration
-        ease: "power4.out", // Smooth easing
-        onComplete: () => {
-          // Reverse animation back to original state
-          gsap.to(".hero h1", {
-            scale: 1, // Back to original size
-            opacity: 1, // Ensure opacity stays visible
-            duration: 1, // Slightly faster for a snappy return
-            ease: "power4.in", // Smooth easing for return
-            onComplete: resolve, // Resolve promise after return animation
-          });
-        },
-      });
-    });
-  }
+  //     // Animate scaling and opacity
+  //     gsap.to(".hero h1", {
+  //       scale: 1.5, // Scale uniformly in all directions
+  //       opacity: 1, // Fade in
+  //       duration: 1, // Animation duration
+  //       ease: "power4.out", // Smooth easing
+  //       onComplete: () => {
+  //         // Reverse animation back to original state
+  //         gsap.to(".hero h1", {
+  //           scale: 1, // Back to original size
+  //           opacity: 1, // Ensure opacity stays visible
+  //           duration: 1, // Slightly faster for a snappy return
+  //           ease: "power4.in", // Smooth easing for return
+  //           onComplete: resolve, // Resolve promise after return animation
+  //         });
+  //       },
+  //     });
+  //   });
+  // }
 });
