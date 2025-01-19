86f11699497d9beda821c942196c6e2d5b426703
chan <penalescjay@gmail.com>
2025-01-20 03:35:23 +0800

    oh

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+});
