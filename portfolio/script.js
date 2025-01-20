b56d38ef26fe89ff47350bb212fefb4a99154013
chan <penalescjay@gmail.com>
2025-01-20 23:42:44 +0800

    j

--- portfolio/script.js
+++ portfolio/script.js
@@ -8,4 +8,46 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   gsap.ticker.lagSmoothing(0);
+
+  const stickySection = document.querySelector(".home");
+  const rows = document.querySelectorAll(".header-row");
+  const socials = document.querySelectorAll(".socials");
+  ScrollTrigger.create({
+    trigger: stickySection,
+    start: "bottom bottom",
+    end: "top bottom",
+    scrub: true,
+    pin: true,
+    pinSpacing: true,
+  });
+
+  socials.forEach((social) => {
+    const letters = social.querySelectorAll(".social-link");
+    gsap.to(letters, {
+      y: -600,
+      stagger: 0.05,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: stickySection,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+  });
+
+  rows.forEach((row) => {
+    const headers = row.querySelectorAll(".intro-header");
+    gsap.to(headers, {
+      x: -600,
+      stagger: 0.4,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: stickySection,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+  });
 });
