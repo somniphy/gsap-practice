b48eef21f6f998fc6129f047faffa57ff8486b17
Christian Penales <penalescjay@gmail.com>
2025-10-06 13:14:38 +0800

    products slider

---
+++ gsap/src/product-list.js
@@ -0,0 +1,69 @@
+const products = [
+  {
+    name: "Hera Chair",
+    img: "/products/product-1.png",
+    price: 240,
+    tag: "Minimalist",
+    url: "https://store.example.com/hera-chair",
+  },
+  {
+    name: "Hades Chair",
+    img: "/products/product-2.png",
+    price: 420,
+    tag: "Minimalist",
+    url: "https://store.example.com/hades-chair",
+  },
+  {
+    name: "Poseidon Chair",
+    img: "/products/product-3.png",
+    price: 400,
+    tag: "Minimalist",
+    url: "https://store.example.com/poseidon-chair",
+  },
+  {
+    name: "Zeus Chair",
+    img: "/products/product-4.png",
+    price: 460,
+    tag: "Minimalist",
+    url: "https://store.example.com/zeus-chair",
+  },
+  {
+    name: "Chrono Chair",
+    img: "/products/product-5.png",
+    price: 240,
+    tag: "Minimalist",
+    url: "https://store.example.com/hera-chair",
+  },
+  {
+    name: "Centaur Chair",
+    img: "/products/product-6.png",
+    price: 240,
+    tag: "Minimalist",
+    url: "https://store.example.com/hera-chair",
+  },
+
+  {
+    name: "Nyx Chair",
+    img: "/products/product-7.png",
+    price: 240,
+    tag: "Minimalist",
+    url: "https://store.example.com/hera-chair",
+  },
+  {
+    name: "Wukong Chair",
+    img: "/products/product-8.png",
+    price: 240,
+    tag: "Minimalist",
+    url: "https://store.example.com/hera-chair",
+  },
+  {
+    name: "Enchantress Chair",
+    img: "/products/product-9.png",
+    price: 240,
+    tag: "Minimalist",
+    url: "https://store.example.com/hera-chair",
+  },
+];
+
+
+export default products;
